5729bd8142d30c87c4f1df7a3717bbdd522e0902
Arturo Herraez <arturocampi.herraezmoreno@gmail.com>
2024-04-07 23:08:52 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
+Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::resource('users', UserController::class)->middleware('auth');
