e46f2e7c37e91d41948452fae2d3670c49a083e6
Arturo Herraez <arturocampi.herraezmoreno@gmail.com>
2024-04-08 15:49:24 +0200

    http scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('app.env') === 'production') {
-            URL::forceScheme('https');
+        // if(config('app.env') === 'production') {
+        //     URL::forceScheme('https');
+        // }
+        if (App::environment('heroku')) {
+            URL::forceScheme('http');
         }
     }
 }
