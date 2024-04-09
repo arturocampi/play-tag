ab5d0cc1f5f22d9bd2f15ec687f16a8fbfaf0e78
Arturo Herraez <arturocampi.herraezmoreno@gmail.com>
2024-04-09 22:52:41 +0200

    service provider

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
@@ -16,3 +17,8 @@ Route::post('register', [RegisterController::class, 'register']);
 Route::get('/', function () { return view('welcome');});
 Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 Route::resource('users', UserController::class)->middleware('auth');
+
+URL::forceScheme('https');
+if (config('app.env') === 'production') {
+    URL::forceScheme('https');
+}
